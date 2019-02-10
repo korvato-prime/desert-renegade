19b4e1a242d06d68c3a751410f4391e1a7dc51c6
CorvatoCafeto <emma.fs92@gmail.com>
2019-02-10 14:36:51 -0300

    Character sprites 4 out of 8 directions

--- source/characters/player/BadassCactus.gd
+++ source/characters/player/BadassCactus.gd
@@ -11,12 +11,16 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("w"):
 		motion += Vector2(0, -1)
+		$Sprite.play("up_right")
 	if Input.is_action_pressed("s"):
 		motion += Vector2(0, 1)
+		$Sprite.play("down_left")
 	if Input.is_action_pressed("a"):
 		motion += Vector2(-1, 0)
+		$Sprite.play("up_left")
 	if Input.is_action_pressed("d"):
 		motion += Vector2(1, 0)
+		$Sprite.play("down_right")
 	
 	motion = motion.normalized() * MOTION_SPEED
 
