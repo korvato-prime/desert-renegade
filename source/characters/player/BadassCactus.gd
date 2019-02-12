2e131902c77de3589d628960378c1e751d1c252a
CorvatoCafeto <emma.fs92@gmail.com>
2019-02-11 22:54:49 -0300

    More tileset upgrades

--- source/characters/player/BadassCactus.gd
+++ source/characters/player/BadassCactus.gd
@@ -61,12 +61,6 @@ func _physics_process(delta):
 		motion += Vector2(-1, 0)
 	if Input.is_action_pressed("d"):
 		motion += Vector2(1, 0)
-	if Input.is_action_pressed("jump"):
-		motion += Vector2(0, -6)
-		#$CollisionShape2D.disabled = true
-		$CollisionShape2D.one_way_collision= true
-		#get_node('/root').collision_layer = Jumping
-		$Timer.start()
 
 	motion = motion.normalized() * MOTION_SPEED
 	move_and_slide(motion)
@@ -77,8 +71,3 @@ func direction2str(direction):
         angle += 2 * PI
     var index = round(angle / PI * 4)
     return directions[index]
-	
-func _on_Timer_timeout():
-	#$CollisionShape2D.disabled = false
-	$CollisionShape2D.one_way_collision= false
-	pass
